f158bed2ef74cd6f599c8f4baa159d9d521ff012
Bilal <bilalnc8@gmail.com>
2022-08-25 22:53:54 -0400

    more info scroll bar

--- src/components/MoreInfo.js
+++ src/components/MoreInfo.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import { Scrollbars } from 'react-custom-scrollbars-2';
+import { useState, useEffect } from 'react';
 
 const MoreInfo = ({movie, movieArray, url}) => {
 
   const [history, setHistory] = useState([])
+  // map
+  // then indivually call things and then append id into the hsitory state
 
   return (
-    <div className='moreInfo'> 
-    // map
-    then indivually call things and then append id into the hsitory state<img  src={`${url}${movieArray[movie].backdrop_path}`} />
-    
+    <div className='moreInfo'>
+    <Scrollbars style={{ width: 1000, height: 800 }}>
+     <div>
+    <img  src={`${url}${movieArray[movie].backdrop_path}`} />
+    </div>
+    </Scrollbars> 
     </div>
   )
 }
